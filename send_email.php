aad78a2f149487293ee798b52423c5e03322c3ec
mohammed-alan <123910224+mohammed-alan@users.noreply.github.com>
2023-08-17 16:45:25 -0400

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -7,11 +7,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $to = "mohd.otmn@gmail.com"; // Replace with your email address
     $subject = "New message from $name";
     $headers = "From: $email";
-
+    
+    // You can add additional headers or modifications here if needed
+    
     if (mail($to, $subject, $message, $headers)) {
         echo "Message sent successfully.";
     } else {
         echo "Error sending the message.";
     }
+} else {
+    echo "Method not allowed.";
 }
 ?>
